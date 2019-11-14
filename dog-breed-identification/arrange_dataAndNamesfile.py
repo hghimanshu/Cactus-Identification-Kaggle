d99e4f3cd27328628022ff50d05fc55ceba81a16
hghimanshu <hghimanshu81@gmail.com>
2019-11-14 11:22:40 +0530

    added names fucntion

--- dog-breed-identification/arrange_dataAndNamesfile.py
+++ dog-breed-identification/arrange_dataAndNamesfile.py
@@ -5,12 +5,14 @@ def makeDataFile(trainfile, testfile, totalimages):
     pass
 
 
-def makeNameFile(clasfolder):
+def makeNameFile(clasfolder, names_path):
     total_classes = os.listdir(clasfolder)
-    with open('classifier.names', 'w') as f:
+    with open(names_path + 'classifier.names', 'w') as f:
         for name in total_classes:
             f.write(name + '\n')
 
 if __name__ == "__main__":
     path = os.getcwd()
-    print(path)
+    data_folder = path + '/dog-breed-identification/data/train/'
+    names_path = path + '/dog-breed-identification/'
+    makeNameFile(data_folder, names_path)
