da2f3aad2a5a80dd11ec5da8960b857748e05699
hghimanshu <hghimanshu81@gmail.com>
2019-11-14 12:18:10 +0530

    added the data file generation script

--- dog-breed-identification/arrange_dataAndNamesfile.py
+++ dog-breed-identification/arrange_dataAndNamesfile.py
@@ -1,12 +1,18 @@
 import os
 
 
-def makeDataFile(trainfile, testfile, totalimages):
-    pass
+def makeDataFile():
+    data_file = path + '/dog-breed-identification/classifier.data'
+    with open(data_file,'w+') as f:
+        f.write('classes='+str(total_classes)+'\n')
+        f.write('train='+trainfile+'\n')
+        f.write('valid='+testfile+'\n')
+        f.write('names='+namesfile+'\n')
+        f.write('backup='+backupfile+'\n')
 
 
-def makeNameFile(clasfolder, names_path):
-    total_classes = os.listdir(clasfolder)
+
+def makeNameFile():
     with open(names_path + 'classifier.names', 'w') as f:
         for name in total_classes:
             f.write(name + '\n')
@@ -15,4 +21,13 @@ if __name__ == "__main__":
     path = os.getcwd()
     data_folder = path + '/dog-breed-identification/data/train/'
     names_path = path + '/dog-breed-identification/'
-    makeNameFile(data_folder, names_path)
+    trainfile = path + '/dog-breed-identification/train.txt'
+    testfile = path + '/dog-breed-identification/test.txt'
+    namesfile = path + '/dog-breed-identification/classifier.names'
+    backupfile = '/home/himanshu/Himanshu/alpr-unconstrained/darknet/backup/'
+    total_classes = os.listdir(data_folder)
+
+
+    makeNameFile()
+    makeNameFile()
+    
